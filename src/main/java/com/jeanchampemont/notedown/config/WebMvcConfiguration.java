616fcd257a7c0c2a8b9deea53452b68b70c92aa9
Jean Champémont <git@jeanchampemont.com>
2014-12-26 20:26:11 +0100

    Internationalization : init + english

--- src/main/java/com/jeanchampemont/notedown/config/WebMvcConfiguration.java
+++ src/main/java/com/jeanchampemont/notedown/config/WebMvcConfiguration.java
@@ -2,9 +2,15 @@ package com.jeanchampemont.notedown.config;
 
 import com.jeanchampemont.notedown.web.SecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
@@ -15,6 +21,20 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(securityInterceptor);
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.ENGLISH);
+        return clr;
+    }
 }
